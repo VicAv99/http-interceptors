45f5b3aff41638a6e6caecc77c95b5fc72dd9b34
Victor Avila <victor.avila@venturplex.com>
2019-02-21 22:39:38 -0700

    create: main layout

--- src/app/core/tasks.service.ts
+++ src/app/core/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Task } from '../tasks/task';
+import { Observable } from 'rxjs';
 
 const model = 'tasks';
 const BASE_URL = 'http://localhost:3000';
@@ -12,24 +13,24 @@ export class TasksService {
 
   constructor(private http: HttpClient) { }
 
-  all() {
-    return this.http.get(`${BASE_URL}/${model}`);
+  all(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${BASE_URL}/${model}`);
   }
 
-  findOne(taskId: number) {
-    return this.http.get(`${BASE_URL}/${model}/${taskId}`);
+  findOne(taskId: number): Observable<Task> {
+    return this.http.get<Task>(`${BASE_URL}/${model}/${taskId}`);
   }
 
-  create(task: Task) {
-    return this.http.post(`${BASE_URL}/${model}`, task);
+  create(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${BASE_URL}/${model}`, task);
   }
 
-  update(task: Task) {
-    return this.http.patch(`${BASE_URL}/${model}/${task.id}`, task);
+  update(task: Task): Observable<Task> {
+    return this.http.patch<Task>(`${BASE_URL}/${model}/${task.id}`, task);
   }
 
-  delete(taskId: number) {
-    return this.http.delete(`${BASE_URL}/${model}/${taskId}`);
+  delete(taskId: number): Observable<Task> {
+    return this.http.delete<Task>(`${BASE_URL}/${model}/${taskId}`);
   }
 
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Task } from './task';
+import { TasksService } from '../core/tasks.service';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  constructor(private tasksService: TasksService) { }
 
   ngOnInit() {
+  }
+
+  getTasks() {
+    this.tasksService.all()
+      .subscribe(res => this.tasks = res);
   }
 
 }
